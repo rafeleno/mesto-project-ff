83ed621088ee8e15bd54b8356729a6a3bee8eb1a
Victor PC <viktorbaskakov356@gmail.com>
2025-01-29 19:52:30 +0300

    continue setting CSS loaders and plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,55 +1,45 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  entry: { main: './src/scripts/index.js' },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js',
-    publicPath: '',
-  },
-  mode: 'development',
-  devServer: {
-    static: path.resolve(__dirname, './dist'), // путь, куда "смотрит" режим разработчика
-    compress: true, // это ускорит загрузку в режиме разработки
-    port: 8080, // порт, чтобы открывать сайт по адресу localhost:8080, но можно поменять порт
+    entry: { main: "./src/scripts/index.js" },
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "main.js",
+        publicPath: "",
+    },
+    mode: "development",
+    devServer: {
+        static: path.resolve(__dirname, "./dist"), // путь, куда "смотрит" режим разработчика
+        compress: true, // это ускорит загрузку в режиме разработки
+        port: 8080, // порт, чтобы открывать сайт по адресу localhost:8080, но можно поменять порт
 
-    open: true, // сайт будет открываться сам при запуске npm run dev
-  },
-  module: {
-    rules: [
-      // rules — это массив правил
-      // добавим в него объект правил для бабеля
-      {
-        test: /\.css$/,
-        // при обработке этих файлов нужно использовать
-        // MiniCssExtractPlugin.loader и css-loader
-        use: [
-          MiniCssExtractPlugin.loader,
-          {
-            loader: 'css-loader',
-          },
+        open: true, // сайт будет открываться сам при запуске npm run dev
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader"],
+            },
+            {
+                test: /\.js$/,
+                use: "babel-loader",
+                exclude: "/node_modules/",
+            },
         ],
-        // регулярное выражение, которое ищет все js файлы
-        test: /\.js$/,
-        // при обработке этих файлов нужно использовать babel-loader
-        use: 'babel-loader',
-        // исключает папку node_modules, файлы в ней обрабатывать не нужно
-        exclude: '/node_modules/',
-      },
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "src/index.html",
+        }),
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin(),
+        new HtmlWebpackPlugin({
+            template: "./src/index.html",
+        }),
+        new CleanWebpackPlugin(), // использовали плагин
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: 'src/index.html',
-    }),
-    new CleanWebpackPlugin(),
-    new MiniCssExtractPlugin(),
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-    }),
-    new CleanWebpackPlugin(), // использовали плагин
-  ],
 };
