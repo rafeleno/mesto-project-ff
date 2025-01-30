e3509ff274bc9e40235a036b91ae4c707e3c4e04
Victor PC <viktorbaskakov356@gmail.com>
2025-01-30 19:04:09 +0300

    Finish the CSS loading!

--- webpack.config.js
+++ webpack.config.js
@@ -11,11 +11,10 @@ module.exports = {
     },
     mode: "development",
     devServer: {
-        static: path.resolve(__dirname, "./dist"), // путь, куда 'смотрит' режим разработчика
-        compress: true, // это ускорит загрузку в режиме разработки
-        port: 8080, // порт, чтобы открывать сайт по адресу localhost:8080, но можно поменять порт
-
-        open: true, // сайт будет открываться сам при запуске npm run dev
+        static: path.resolve(__dirname, "./dist"),
+        compress: true,
+        port: 8080,
+        open: true,
     },
     module: {
         rules: [
@@ -24,12 +23,8 @@ module.exports = {
                 use: [MiniCssExtractPlugin.loader, "css-loader"],
             },
             {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                use: [
-                    {
-                        loader: "file-loader",
-                    },
-                ],
+                test: /\.(png|jpe?g|gif|svg)$/i,
+                type: "asset/resource",
             },
             {
                 test: /\.js$/,
@@ -41,18 +36,12 @@ module.exports = {
                     },
                 },
             },
-            {
-                test: /\.html$/,
-                use: ["html-loader"],
-            },
         ],
     },
     plugins: [
         new HtmlWebpackPlugin({
             template: "./src/index.html",
         }),
-        new MiniCssExtractPlugin({
-            filename: "[name].css",
-        }),
+        new MiniCssExtractPlugin(),
     ],
 };
