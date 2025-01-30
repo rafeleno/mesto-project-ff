b44b584204b52ca50dcbc9220d788169234c59b4
Victor PC <viktorbaskakov356@gmail.com>
2025-01-30 19:35:08 +0300

    feat: добавление стилей для аватара и обновление разметки профиля

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,38 +2,38 @@ import "../pages/index.css";
 // @todo: Темплейт карточки
 const placesList = document.querySelector(".places__list");
 
-const addIcon = new URL("./images/add-icon.svg", import.meta.url);
-const avatar = new URL("./images/avatar.jpg", import.meta.url);
-const card_1 = new URL("./images/card_1", import.meta.url);
-const card_2 = new URL("./images/card_2", import.meta.url);
-const card_3 = new URL("./images/card_3", import.meta.url);
-const card_4 = new URL("./images/card_4", import.meta.url);
-const card_5 = new URL("./images/card_5", import.meta.url);
-const card_6 = new URL("./images/card_6", import.meta.url);
-const close = new URL("./images/close.svg", import.meta.url);
-const deleteIcon = new URL("./images/delete-icon.svg", import.meta.url);
-const editIcon = new URL("./images/edit-icon.svg", import.meta.url);
-const likeActivate = new URL("./images/like-activate.svg", import.meta.url);
-const likeInActivate = new URL("./images/like-inactivate.svg", import.meta.url);
-const logo = new URL("./images/logo.svg", import.meta.url);
+// const addIcon = new URL("./images/add-icon.svg", import.meta.url);
+// const avatar = new URL("./images/avatar.jpg", import.meta.url);
+// const card_1 = new URL("./images/card_1", import.meta.url);
+// const card_2 = new URL("./images/card_2", import.meta.url);
+// const card_3 = new URL("./images/card_3", import.meta.url);
+// const card_4 = new URL("./images/card_4", import.meta.url);
+// const card_5 = new URL("./images/card_5", import.meta.url);
+// const card_6 = new URL("./images/card_6", import.meta.url);
+// const close = new URL("./images/close.svg", import.meta.url);
+// const deleteIcon = new URL("./images/delete-icon.svg", import.meta.url);
+// const editIcon = new URL("./images/edit-icon.svg", import.meta.url);
+// const likeActivate = new URL("./images/like-activate.svg", import.meta.url);
+// const likeInActivate = new URL("./images/like-inactivate.svg", import.meta.url);
+// const logo = new URL("./images/logo.svg", import.meta.url);
 
-const images = [
-    // меняем исходные пути на переменные
-    { name: "addIcon", link: addIcon },
-    { name: "avatar", link: avatar },
-    { name: "card_1", link: card_1 },
-    { name: "card_2", link: card_2 },
-    { name: "card_3", link: card_3 },
-    { name: "card_4", link: card_4 },
-    { name: "card_5", link: card_5 },
-    { name: "card_6", link: card_6 },
-    { name: "close", link: close },
-    { name: "deleteIcon", link: deleteIcon },
-    { name: "editIcon", link: editIcon },
-    { name: "likeActivate", link: likeActivate },
-    { name: "likeInActivate", link: likeInActivate },
-    { name: "logo", link: logo },
-];
+// const images = [
+//     // меняем исходные пути на переменные
+//     { name: "addIcon", link: addIcon },
+//     { name: "avatar", link: avatar },
+//     { name: "card_1", link: card_1 },
+//     { name: "card_2", link: card_2 },
+//     { name: "card_3", link: card_3 },
+//     { name: "card_4", link: card_4 },
+//     { name: "card_5", link: card_5 },
+//     { name: "card_6", link: card_6 },
+//     { name: "close", link: close },
+//     { name: "deleteIcon", link: deleteIcon },
+//     { name: "editIcon", link: editIcon },
+//     { name: "likeActivate", link: likeActivate },
+//     { name: "likeInActivate", link: likeInActivate },
+//     { name: "logo", link: logo },
+// ];
 
 function handleCardDelete(evt) {
     const card = evt.target.closest(".card");
