61c2fdaa2be8ea3b074420b80dafa5e6079f18db
Victor Baskakov <viktorbaskakov356@gmail.com>
2025-02-10 16:00:36 +0300

    do Funcs

--- src/scripts/components/modal.js
+++ src/scripts/components/modal.js
@@ -1,25 +1,56 @@
-const forms = document.querySelectorAll(".popup__form");
+const forms = document.querySelectorAll('.popup__form');
 forms.forEach((form) => {
-    form.addEventListener("submit", (evt) => {
-        evt.preventDefault();
-        form.closest(".popup").close();
-    });
+  form.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+    form.closest('.popup').close();
+  });
 });
 
 // const profileEditForm = document.querySelector("#profile-edit-form");
 // let profileEditFormName = "Жак-Ив Кусто";
 // let profileEditFormEmployment = "Исследователь океана";
 
-const openModal = (openButton, modal) => {
-    openButton.addEventListener("click", (evt) => {
-        modal.showModal();
-    });
+// const openModal = (openButton, modal) => {
+//   openButton.addEventListener('click', (evt) => {
+//     modal.classList.add('.popup_is-opened');
+//     modal.showModal();
+//   });
+// };
+
+// const closeModal = (closeButton, modal) => {
+//   closeButton.addEventListener('click', (evt) => {
+//     modal.classList.remove('.popup_is-open');
+//     modal.close();
+//   });
+// };
+
+const closeModalEsc = (evt) => {
+  if (evt.key === 'Escape') {
+    const popup = document.querySelector('.popup_is-open');
+    popup.classList.remove('.popup_is-open');
+    popup.close();
+  }
 };
 
-const closeModal = (closeButton, modal) => {
-    closeButton.addEventListener("click", (evt) => {
-        modal.close();
-    });
+const closePopupOverlay = (evt) => {
+  if (evt.target.classList.contains('.popup')) {
+    evt.target.classList.remove('.popup_is-open');
+    evt.target.close();
+  }
 };
 
-export { openModal, closeModal };
+//функция открытия, если попап открыт, добавляем слушателя клавиатуры
+const openModal = (modal) => {
+  modal.classList.add('popup_opened');
+  document.addEventListener('keydown', closeModalEsc);
+  modal.addEventListener('click', closePopupOverlay);
+};
+
+//функция закрытия, если попап закрыт, то удаляем слушателя клавиатуры
+const closeModal = (modal) => {
+  smodal.classList.remove('popup_opened');
+  document.removeEventListener('keydown', closePopupEsc);
+  modal.removeEventListener('click', closePopupOverlay);
+};
+
+export { openModal, closeModal, closeModalEsc, closePopupOverlay };
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,24 +1,35 @@
-import { presets } from "../../babel.config.js";
-import "../pages/index.css";
-import { openModal, closeModal } from "./components/modal.js";
-import { initialCards, handleCardDelete, createCard } from "./components/cards.js";
+import { presets } from '../../babel.config.js';
+import '../pages/index.css';
+import {
+  openModal,
+  closeModal,
+  closeModalEsc,
+  closePopupOverlay,
+} from './components/modal.js';
+import {
+  initialCards,
+  handleCardDelete,
+  createCard,
+} from './components/cards.js';
 
-const placesList = document.querySelector(".places__list");
-const closeButtons = document.querySelectorAll(".popup__close");
-const profileAddButton = document.querySelector("#profile-add-button");
-const addCartDialog = document.querySelector("#add-cart-dialog");
-const profileEditButton = document.querySelector("#profile-edit-button");
-const popupTypeEdit = document.querySelector("#popup-type-edit");
+const placesList = document.querySelector('.places__list');
+const closeButtons = document.querySelectorAll('.popup__close');
+const profileAddButton = document.querySelector('#profile-add-button');
+const addCartDialog = document.querySelector('#add-cart-dialog');
+const profileEditButton = document.querySelector('#profile-edit-button');
+const popupTypeEdit = document.querySelector('#popup-type-edit');
 
 openModal(profileAddButton, addCartDialog);
 openModal(profileEditButton, popupTypeEdit);
 
 closeButtons.forEach((button) => {
-    closeModal(button, button.closest(".popup"));
+  closeModal(button, button.closest('.popup'));
 });
 
 initialCards.forEach((item) => {
-    placesList.append(createCard(item.src, item.alt, item.name, handleCardDelete));
+  placesList.append(
+    createCard(item.src, item.alt, item.name, handleCardDelete)
+  );
 });
 
 console.log(addCartDialog);
