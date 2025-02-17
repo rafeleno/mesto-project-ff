56c90d8e9256c4941fddd5f979cd3968cbdf8bc2
Victor PC <viktorbaskakov356@gmail.com>
2025-02-17 21:16:04 +0300

    fix; A Lot Of Changes - refactoring

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -6,36 +6,49 @@ import { handleCardDelete, createCard, likeButtonHandleClick } from "./component
 const placesList = document.querySelector(".places__list");
 const closeButtons = document.querySelectorAll(".popup__close");
 const profileAddButton = document.querySelector(".profile__add-button");
-const addCardPopup = document.querySelector(".popup_type_new-card");
-const addCardForm = addCardPopup.querySelector(".popup__form");
-const popupTypeEdit = document.querySelector(".popup_type_edit");
 const profileEditButton = document.querySelector("#profile-edit-button");
-const cardAddFormElement = document.querySelector("#card-add-form");
+const popupTypeAddCard = document.querySelector(".popup_type_new-card");
+const popupTypeEdit = document.querySelector(".popup_type_edit");
+const addCardForm = popupTypeAddCard.querySelector("#card-add-form");
 const popups = document.querySelectorAll(".popup");
+
+const nameInput = popupTypeEdit.querySelector(".popup__input_type_name");
+const jobInput = popupTypeEdit.querySelector(".popup__input_type_description");
+const profileTitle = document.querySelector(".profile__title");
+const profileDescription = document.querySelector(".profile__description");
 
 const imagePopup = document.querySelector(".popup_type_image");
 const popupImageElement = document.querySelector(".popup__image");
 const popupCaptionElement = document.querySelector(".popup__caption");
 
+const imageSrcInput = document.querySelector(".popup__input_type_url");
+const imageNameInput = document.querySelector(".popup__input_type_card-name");
+
 // Добавление открытия Popup'a при нажатии на соответсвующие кнопки
-profileEditButton.addEventListener("click", (evt) => openModal(popupTypeEdit));
-profileAddButton.addEventListener("click", (evt) => openModal(addCardPopup));
+profileEditButton.addEventListener("click", popupEditOpen);
+profileAddButton.addEventListener("click", popupTypeAddCardOpen);
+
+// логика Для popupTypeEdit
+function popupEditOpen(evt) {
+  openModal(popupTypeEdit);
+
+  nameInput.value = profileTitle.textContent;
+  jobInput.value = profileDescription.textContent;
+}
+// логика Для popupTypeAddCard
+function popupTypeAddCardOpen(evt) {
+  openModal(popupTypeAddCard);
+
+  addCardForm.reset();
+}
 
 //закрытие popup по кажатию на overlay
-document.addEventListener("mousedown", function (evt) {
-  if (evt.target.classList.contains("popup_is-opened")) {
-    popups.forEach((popup) => {
-      popup.classList.remove("popup_is-opened");
-    });
-  }
-  if (evt.target.classList.contains("popup_type_edit")) {
-    // Так я изолизую функцию от остальных элементов
-    resetProfileForm();
-  }
-  if (evt.target.classList.contains("popup_type_new-card")) {
-    // Тоже самое
-    cardAddFormElement.reset();
-  }
+popups.forEach((popup) => {
+  popup.addEventListener("mousedown", (evt) => {
+    if (evt.target.classList.contains("popup_is-opened")) {
+      closeModal(evt.target);
+    }
+  });
 });
 
 // Функция открытия карточки
@@ -49,38 +62,13 @@ const popupOpener = (evt) => {
 
 // Закрытия нажатием на кнопки закрытия.
 closeButtons.forEach((button) => {
+  const popup = button.closest(".popup");
   button.addEventListener("click", (evt) => {
-    closeModal(button.closest(".popup"));
-    if (button.closest(".popup").classList.contains("popup_type_edit")) {
-      resetProfileForm();
-    }
-    if (button.closest(".popup").classList.contains("popup_type_new-card")) {
-      cardAddFormElement.reset();
-    }
+    closeModal(popup);
   });
 });
 
-// TODO: Придумать как сбрасывать формы на Esc "По правилам"
-// popupTypeEdit.addEventListener("keydown", (evt) => {
-//   if (evt.key === "Escape" || evt.key === "Esc") {
-//     resetProfileForm();
-//   }
-// });
-// addCardPopup.addEventListener("keydown", (evt) => {
-//   if (evt.key === "Escape" || evt.key === "Esc") {
-//     cardAddFormElement.reset();
-//   }
-// });
-
-// TODO:Привести в порядок, проверить на уязвимость формы -------------------------------------
 // Обрабытваем Submit в редакторе профиля -----------------------------------
-
-const profileEditFormElement = popupTypeEdit.querySelector(".popup__form");
-const nameInput = profileEditFormElement.querySelector(".popup__input_type_name");
-const jobInput = profileEditFormElement.querySelector(".popup__input_type_description");
-const profileTitle = document.querySelector(".profile__title");
-const profileDescription = document.querySelector(".profile__description");
-
 function handleProfileFormSubmit(evt) {
   evt.preventDefault();
 
@@ -89,45 +77,26 @@ function handleProfileFormSubmit(evt) {
 
   profileTitle.textContent = name;
   profileDescription.textContent = job;
-
-  resetProfileForm;
+  // resetProfileForm();
   closeModal(popupTypeEdit);
 }
 
 // Вызываем на функцию submit'a на форму редактирования профиля
-profileEditFormElement.addEventListener("submit", handleProfileFormSubmit);
-
-// Сброс полей формы редактирования профиля
-const resetProfileForm = () => {
-  nameInput.value = profileTitle.textContent;
-  jobInput.value = profileDescription.textContent;
-};
-
-// Вносим значение имени и рода деятельности в импуты на этапе загрузки страницы
-resetProfileForm();
+popupTypeEdit.addEventListener("submit", handleProfileFormSubmit);
 
 // Обрабатывает создание новой карточки
-const imageSrcInput = document.querySelector(".popup__input_type_url");
-const imageNameInput = document.querySelector(".popup__input_type_card-name");
-
 function handleAddCardFormSubmit(evt) {
   evt.preventDefault();
 
   const imageSrc = imageSrcInput.value;
   const imageName = imageNameInput.value;
 
-  placesList.prepend(createCard(imageSrc, imageName, handleCardDelete, likeButtonHandleClick));
+  closeModal(popupTypeAddCard);
 
-  cardAddFormElement.reset();
+  placesList.prepend(createCard(imageSrc, imageName, handleCardDelete, likeButtonHandleClick));
 }
 
-cardAddFormElement.addEventListener("submit", handleAddCardFormSubmit);
-
-// Логика работы submit для addCardPopup
-addCardForm.addEventListener("submit", (evt) => {
-  cardAddFormElement.reset();
-  closeModal(addCardPopup);
-});
+addCardForm.addEventListener("submit", handleAddCardFormSubmit);
 
 // Добавление изначальных карточек на страницу
 initialCards.forEach((item) => {
