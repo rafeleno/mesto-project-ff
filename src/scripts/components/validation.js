5cb5b3b7b1ec1cba194c19e6db6254c139cfbb6e
Victor PC <viktorbaskakov356@gmail.com>
2025-03-03 11:51:12 +0300

    in process

--- src/scripts/components/validation.js
+++ src/scripts/components/validation.js
@@ -1,6 +1,20 @@
 const formIsValid = (form) => {
   const inputs = form.querySelectorAll(".popup__input");
-  if (inputs.some((input) => !input.validState.valid)) {
-    return false;
+  if (inputs.some((input) => input.validity.valid)) {
+    return true;
   }
+};
+
+nameInputIsValid = (nameInput) => {};
+
+aboutInputIsValid = (aboutInput) => {};
+
+defaultInputIsValid = (defaultInput) => {};
+
+showInputError = (error) => {
+  error.classList.add(".popup__input__error-ia-active");
+};
+
+hideInputError = (error) => {
+  error.classList.remove(".popup__input__error-ia-active");
 };
