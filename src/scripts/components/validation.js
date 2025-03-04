5ea196b592f96005394b133be00fcc8251794633
Victor PC <viktorbaskakov356@gmail.com>
2025-03-04 21:14:14 +0300

    add disabled effect

--- src/scripts/components/validation.js
+++ src/scripts/components/validation.js
@@ -1,5 +1,5 @@
 const formIsValid = (form) => {
-  const inputs = form.querySelectorAll('.popup__input');
+  const inputs = form.querySelectorAll(".popup__input");
   if (inputs.some((input) => input.validity.valid)) {
     return true;
   }
@@ -38,71 +38,95 @@ const formIsValid = (form) => {
 
 const nameInputIsValid = (nameInput) => {
   const regex = /^[a-zA-Zа-яА-ЯёЁ\s-]+$/;
-  const error = nameInput.nextElementSibling; // TODO: Переделать путь
+  const error = nameInput.nextElementSibling;
+  const submitButton = nameInput.closest(".popup__form").querySelector(".popup__button");
+  // TODO: Переделать путь (по id, с сокращением серез `${id}`)
+  // TODO: Переделать позиционирование
+  // TODO: Переделать так, чтобы память не утекала
 
   // Сбрасываем предыдущую ошибку
-  nameInput.setCustomValidity('');
-  error.textContent = '';
+  nameInput.setCustomValidity("");
+  error.textContent = "example";
+  submitButton.disabled = false;
 
   // Проверяем на отсутсвие текста
   if (nameInput.value.length === 0) {
-    nameInput.setCustomValidity('Вы пропустили это поле.');
+    nameInput.setCustomValidity("Вы пропустили это поле.");
   }
   // Проверяем соответствие шаблону
   else if (!regex.test(nameInput.value)) {
-    nameInput.setCustomValidity(
-      'Текст может содержать только буквы, дефисы и пробелы.'
-    );
+    nameInput.setCustomValidity("Текст может содержать только буквы, дефисы и пробелы.");
   }
   // Проверяем минимальную длину
   else if (nameInput.value.length < 2) {
-    nameInput.setCustomValidity(
-      `Минимальное количество символов: 2. Длина текста сейчас: ${nameInput.value.length}`
-    );
+    nameInput.setCustomValidity(`Минимальное количество символов: 2. Длина текста сейчас: ${nameInput.value.length}`);
   }
   // Проверяем максимальную длину
   else if (nameInput.value.length > 40) {
-    nameInput.setCustomValidity(
-      `Максимальное количество символов: 40. Длина текста сейчас: ${nameInput.value.length}`
-    );
+    nameInput.setCustomValidity(`Максимальное количество символов: 40. Длина текста сейчас: ${nameInput.value.length}`);
   }
 
   // Показываем или скрываем ошибку
   if (nameInput.validationMessage) {
-    error.classList.add('popup__input__error-ia-active');
+    error.classList.add("popup__input__error-ia-active");
     error.textContent = nameInput.validationMessage;
+    submitButton.disabled = true;
   } else {
-    error.classList.remove('popup__input__error-ia-active');
+    error.classList.remove("popup__input__error-ia-active");
   }
 };
 
 const aboutInputIsValid = (aboutInput) => {
   const regex = /^[a-zA-Zа-яА-ЯёЁ\s-]+$/;
-  return (
-    (nameInput.value.length < 2 || nameInput.value.length > 200) &&
-    nameInput.value !== regex
-  );
+  const error = aboutInput.nextElementSibling;
+  const submitButton = aboutInput.closest(".popup__form").querySelector(".popup__button");
+  // TODO: Переделать путь (по id, с сокращением серез `${id}`)
+  // TODO: Переделать позиционирование
+  // TODO: Переделать так, чтобы память не утекала
+
+  // Сбрасываем предыдущую ошибку
+  aboutInput.setCustomValidity("");
+  error.textContent = "example";
+  submitButton.disabled = false;
+
+  // Проверяем на отсутсвие текста
+  if (aboutInput.value.length === 0) {
+    aboutInput.setCustomValidity("Вы пропустили это поле.");
+  }
+  // Проверяем соответствие шаблону
+  else if (!regex.test(aboutInput.value)) {
+    aboutInput.setCustomValidity("Текст может содержать только буквы, дефисы и пробелы.");
+  }
+  // Проверяем минимальную длину
+  else if (aboutInput.value.length < 2) {
+    aboutInput.setCustomValidity(`Минимальное количество символов: 2. Длина текста сейчас: ${aboutInput.value.length}`);
+  }
+  // Проверяем максимальную длину
+  else if (aboutInput.value.length > 200) {
+    aboutInput.setCustomValidity(`Максимальное количество символов: 200. Длина текста сейчас: ${aboutInput.value.length}`);
+  }
+
+  if (aboutInput.validationMessage) {
+    error.classList.add("popup__input__error-ia-active");
+    error.textContent = aboutInput.validationMessage;
+    submitButton.disabled = true;
+  } else {
+    error.classList.remove("popup__input__error-ia-active");
+  }
 };
 
 const placeNameInputIsValid = (placeNameInput) => {};
 
 const linkInputIsValid = (linkInput) => {};
 
-const enableValidation = ({
-  formSelector,
-  inputSelector,
-  submitButtonSelector,
-  inactiveButtonClass,
-  inputErrorClass,
-  errorClass,
-}) => {};
+const enableValidation = ({ formSelector, inputSelector, submitButtonSelector, inactiveButtonClass, inputErrorClass, errorClass }) => {};
 
 const showInputError = (error) => {
-  error.classList.add('.popup__input__error-ia-active');
+  error.classList.add(".popup__input__error-ia-active");
 };
 
 const hideInputError = (error) => {
-  error.classList.remove('.popup__input__error-ia-active');
+  error.classList.remove(".popup__input__error-ia-active");
 };
 
-export { nameInputIsValid };
+export { nameInputIsValid, aboutInputIsValid };
